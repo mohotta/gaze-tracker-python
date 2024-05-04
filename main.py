10d773bbe38f2908935fab901b3d25069e36a0d1
Kumudu Mohottala <kumudulaksitha@gmail.com>
2024-05-04 22:06:31 +0530

    added only x values

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 
 # i = 0
 # csv_file = open("data.csv", 'a')
-# label = 'no'
+# label = 'up/down'
 
 # for _ in range(100000000):
 #     pass
@@ -50,30 +50,30 @@ while True:
         # eye1 - measures
         eye_1_w = np.abs(landmarks[33].x - landmarks[133].x)
         eye_1_h = np.abs(landmarks[159].y - landmarks[145].y)
-        eye_1_x_left = np.abs(landmarks[33].x - landmarks[471].x)/(eye_1_w*dist_z)
-        eye_1_x_right = np.abs(landmarks[133].x - landmarks[469].x)/(eye_1_w*dist_z)
-        eye_1_y_top = np.abs(landmarks[159].y - landmarks[470].y)/(eye_1_h*dist_z)
-        eye_1_y_bottom = np.abs(landmarks[145].y - landmarks[472].y)/(eye_1_h*dist_z)
+        eye_1_x_left = np.abs(landmarks[33].x - landmarks[471].x)/(eye_1_w)
+        eye_1_x_right = np.abs(landmarks[133].x - landmarks[469].x)/(eye_1_w)
+        eye_1_y_top = np.abs(landmarks[159].y - landmarks[470].y)/(eye_1_h)
+        eye_1_y_bottom = np.abs(landmarks[145].y - landmarks[472].y)/(eye_1_h)
 
         print("Eye 1: ", eye_1_x_left, eye_1_x_right, eye_1_y_top, eye_1_y_bottom)
 
         # eye2 - measures
         eye_2_w = np.abs(landmarks[362].x - landmarks[263].x)
         eye_2_h = np.abs(landmarks[386].y - landmarks[374].y)
-        eye_2_x_left = np.abs(landmarks[362].x - landmarks[476].x)/(eye_2_w*dist_z)
-        eye_2_x_right = np.abs(landmarks[263].x - landmarks[474].x)/(eye_2_w*dist_z)
-        eye_2_y_top = np.abs(landmarks[386].y - landmarks[475].y)/(eye_2_h*dist_z)
-        eye_2_y_bottom = np.abs(landmarks[374].y - landmarks[477].y)/(eye_2_h*dist_z)
+        eye_2_x_left = np.abs(landmarks[362].x - landmarks[476].x)/(eye_2_w)
+        eye_2_x_right = np.abs(landmarks[263].x - landmarks[474].x)/(eye_2_w)
+        eye_2_y_top = np.abs(landmarks[386].y - landmarks[475].y)/(eye_2_h)
+        eye_2_y_bottom = np.abs(landmarks[374].y - landmarks[477].y)/(eye_2_h)
 
         print("Eye 2: ", eye_2_x_left, eye_2_x_right, eye_2_y_top, eye_2_y_bottom)
 
         # face
         face_h_width = np.abs(landmarks[366].x - landmarks[137].x)
-        face_left_width = np.abs(landmarks[4].x - landmarks[137].x)/(face_h_width*dist_z)
-        face_right_width = np.abs(landmarks[4].x - landmarks[366].x)/(face_h_width*dist_z)
+        face_left_width = np.abs(landmarks[4].x - landmarks[137].x)/(face_h_width)
+        face_right_width = np.abs(landmarks[4].x - landmarks[366].x)/(face_h_width)
         face_v_height = np.abs(landmarks[10].x - landmarks[152].x)
-        face_top_width = np.abs(landmarks[4].y - landmarks[10].y)/(face_v_height*dist_z)
-        face_bottom_width = np.abs(landmarks[4].y - landmarks[152].y)/(face_v_height*dist_z)
+        face_top_width = np.abs(landmarks[4].y - landmarks[10].y)/(face_v_height)
+        face_bottom_width = np.abs(landmarks[4].y - landmarks[152].y)/(face_v_height)
 
         print("face", face_left_width, face_right_width, face_top_width, face_bottom_width)
 
@@ -81,13 +81,13 @@ while True:
         # csv_file.write(f"{eye_1_x_left},{eye_1_x_right},{eye_1_y_top},{eye_1_y_bottom},{eye_2_x_left},{eye_2_x_right},{eye_2_y_top},{eye_2_y_bottom},{face_left_width},{face_right_width},{face_top_width},{face_bottom_width},{label}\n")
 
         # i += 1
-        # if i >= 1000:
+        # if i >= 100:
         #     csv_file.close()
         #     playsound("sound.wav")
         #     break
 
         model = pickle.load(open('model.pkl', 'rb'))
-        pred = model.predict(np.array([eye_1_x_left, eye_1_x_right, eye_1_y_top, eye_1_y_bottom, eye_2_x_left, eye_2_x_right, eye_2_y_top, eye_2_y_bottom, face_left_width, face_right_width, face_top_width, face_bottom_width]).reshape(1, -1))
+        pred = model.predict(np.array([eye_1_x_left, eye_1_x_right, eye_2_x_left, eye_2_x_right]).reshape(1, -1))
 
         if pred == [0.0]:
             playsound('sound.wav')
